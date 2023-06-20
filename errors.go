78c7820bbd58d2bfb82f1c064087d59f908f5052
Mike Nystoriak <64051911+mnys176@users.noreply.github.com>
2023-06-20 19:14:47 -0400

    Revert "Errors don't need to have a context"
    This reverts commit 1e491f0313d1280833e6947a781ce5948cc7e2e8.

--- errors.go
+++ errors.go
@@ -1,13 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 type UsageError struct {
-	Msg string
+	Context string
+	Err     error
 }
 
 func (e UsageError) Error() string {
-	return "usage: " + e.Msg
+	return fmt.Errorf("%s: %w", e.Context, e.Err).Error()
 }
 
 func (e UsageError) Is(target error) bool {
 	return e.Error() == target.Error()
+}
+
+func (e UsageError) Unwrap() error {
+	return e.Err
 }
--- errors_test.go
+++ errors_test.go
@@ -12,8 +12,11 @@ type usageErrorErrorTester struct {
 
 func (tester usageErrorErrorTester) assertString() func(*testing.T) {
 	return func(t *testing.T) {
-		_, msg, _ := strings.Cut(tester.oString, ": ")
-		sampleUsageError := &UsageError{msg}
+		context, err, _ := strings.Cut(tester.oString, ": ")
+		sampleUsageError := &UsageError{
+			Context: context,
+			Err:     errors.New(err),
+		}
 		if got := sampleUsageError.Error(); got != tester.oString {
 			t.Errorf("error is %q but should be %q", got, tester.oString)
 		}
@@ -27,26 +30,61 @@ type usageErrorIsTester struct {
 
 func (tester usageErrorIsTester) assertBool() func(*testing.T) {
 	return func(t *testing.T) {
-		sampleUsageError := &UsageError{"foo"}
+		sampleUsageError := &UsageError{
+			Context: "foo",
+			Err:     errors.New("same"),
+		}
 		if got := sampleUsageError.Is(tester.iTarget); got != tester.oBool {
 			t.Errorf("result is %t but should be %t", got, tester.oBool)
 		}
 	}
 }
 
+type usageErrorUnwrapTester struct {
+	oErr error
+}
+
+func (tester usageErrorUnwrapTester) assertError() func(*testing.T) {
+	return func(t *testing.T) {
+		sampleUsageError := &UsageError{
+			Context: "foo",
+			Err:     tester.oErr,
+		}
+		if got := sampleUsageError.Unwrap(); !errors.Is(got, tester.oErr) {
+			t.Errorf("error is %q but should be %q", got.Error(), tester.oErr.Error())
+		}
+	}
+}
+
+func (tester usageErrorUnwrapTester) assertNil() func(*testing.T) {
+	return func(t *testing.T) {
+		sampleUsageError := &UsageError{Context: "foo"}
+		if got := sampleUsageError.Unwrap(); got != nil {
+			t.Errorf("error is %q but should be nil", got.Error())
+		}
+	}
+}
+
 func TestUsageErrorError(t *testing.T) {
 	t.Run("baseline", usageErrorErrorTester{
-		oString: "usage: foo",
+		oString: "foo: bar",
 	}.assertString())
 }
 
 func TestUsageErrorIs(t *testing.T) {
 	t.Run("baseline", usageErrorIsTester{
-		iTarget: errors.New("usage: foo"),
+		iTarget: errors.New("foo: same"),
 		oBool:   true,
 	}.assertBool())
 	t.Run("is not", usageErrorIsTester{
-		iTarget: errors.New("usage: bar"),
+		iTarget: errors.New("foo: different"),
 		oBool:   false,
 	}.assertBool())
+}
+
+func TestUsageErrorUnwrap(t *testing.T) {
+	t.Run("baseline", usageErrorUnwrapTester{
+		oErr: errors.New("bar"),
+	}.assertError())
+	t.Run("nil wrapped error", usageErrorUnwrapTester{}.assertNil())
 }
